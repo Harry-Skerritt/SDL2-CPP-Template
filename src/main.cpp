54777e3efaf0e89234587300cf80aa4381c31dc9
Harry Skerritt <harry2.skerritt@live.uwe.ac.uk>
2026-03-09 19:09:45 +0000

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,13 @@
 #define SCREEN_HEIGHT 720
 
 int main() {
+    // SDL Initialisation
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
         return EXIT_FAILURE;
     }
 
+    // Window Creation
     SDL_Window* window = SDL_CreateWindow(
         "SDL2 Window",
         SDL_WINDOWPOS_CENTERED,
@@ -19,16 +21,26 @@ int main() {
         SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_MOUSE_FOCUS
     );
 
+    // Window Fail Safe
     if (window == nullptr) {
         std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return EXIT_FAILURE;
     }
 
+    // Renderer Creation
+    SDL_Renderer* renderer = SDL_CreateRenderer(
+       window,
+       -1,
+       SDL_RENDERER_ACCELERATED
+   );
+
+
     bool is_running = true;
     SDL_Event event;
 
     while (is_running) {
+        // Event Handling
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
                 is_running = false;
@@ -36,8 +48,12 @@ int main() {
         }
 
         // Render Code
+        SDL_SetRenderDrawColor(renderer, 100, 149, 237, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
     }
 
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
